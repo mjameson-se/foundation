7865f179892d8116513d7c8227934c9773deebd5
Michael Jameson <michael.jameson@schneider-electric.com>
2017-07-10 11:55:05 -0700

    Fix issue with classpath search

--- org.f8n.r8n/src/org/f8n/r8n/ClasspathSearch.java
+++ org.f8n.r8n/src/org/f8n/r8n/ClasspathSearch.java
@@ -5,11 +5,15 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.reflect.ClassPath;
 import com.google.common.reflect.ClassPath.ClassInfo;
 
 public class ClasspathSearch
 {
+  private static final Logger LOG = LoggerFactory.getLogger(ClasspathSearch.class);
   private Set<ClassInfo> ci = new HashSet<>();
   private Stream<ClassInfo> stream = ci.stream();
   private final ClassLoader cl;
@@ -48,8 +52,23 @@ public class ClasspathSearch
     return this;
   }
 
+  private Stream<Class<?>> loadClass(ClassInfo classInfo)
+  {
+    try
+    {
+      return Stream.of(classInfo.load());
+    }
+    catch (NoClassDefFoundError ex)
+    {
+      // When attempt to load classes fairly indiscriminately, it is common to fail because a dependency
+      // is missing that is not necessary for program execution.
+      LOG.debug("Failed to load class {}", classInfo.getName());
+      return Stream.of();
+    }
+  }
+
   public ClassStream classStream()
   {
-    return new ClassStream(stream.map(ClassInfo::load));
+    return new ClassStream(stream.flatMap(this::loadClass));
   }
 }
